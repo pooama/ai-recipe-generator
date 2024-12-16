3a45aa41ac409686e65e148445fd99f9b65f20df
poonamabhimanyu2012 <poonamabhimanyu2012@gmail.com>
2024-12-16 15:57:47 +0000

    Amplify Data

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,18 +1,22 @@
 import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
 import { postConfirmation } from "../auth/post-confirmation/resource";
 
-const schema = a
-  .schema({
-    UserProfile: a
-      .model({
-        email: a.string(),
-        profileOwner: a.string(),
-      })
-      .authorization((allow) => [
-        allow.ownerDefinedIn("profileOwner"),
-      ]),
-  })
-  .authorization((allow) => [allow.resource(postConfirmation)]);
+const schema = a.schema({
+  BedrockResponse: a.customType({
+    body: a.string(),
+    error: a.string(),
+  }),
+
+  askBedrock: a
+    .query()
+    .arguments({ ingredients: a.string().array() })
+    .returns(a.ref("BedrockResponse"))
+    .authorization((allow) => [allow.authenticated()])
+    .handler(
+      a.handler.custom({ entry: "./bedrock.js", dataSource: "bedrockDS" })
+    ),
+});
+
 export type Schema = ClientSchema<typeof schema>;
 
 export const data = defineData({
@@ -24,4 +28,3 @@ export const data = defineData({
     },
   },
 });
-
